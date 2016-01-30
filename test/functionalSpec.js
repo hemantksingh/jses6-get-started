26a316f0fbcca2b15a4022456175d908499da56b
hemantksingh <hemantksingh.hk@gmail.com>
2016-01-30 18:17:32 +0000

    Uses chai expect for test assertions.

--- test/functionalSpec.js
+++ test/functionalSpec.js
@@ -1,16 +1,17 @@
-import assert from 'assert';
+import { expect } from 'chai';
 
 describe('Array indexes', () => {
-  describe('#indexOf()', () => {
-    
-    it('returns -1 when the value is not present', () => {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
+	let array = [1,2,3];
 
-    it('returns 0 when index of first value is queried', () => {
-      assert.equal(0, [1,2,3].indexOf(1));
-    });
+	describe('#indexOf()', () => {
 
-  });
+		it('returns -1 when the value is not present', () => {
+			expect(array.indexOf(5)).to.eq(-1);
+			expect(array.indexOf(0)).to.eq(-1);
+		});
+
+		it('returns 0 when index of first value is queried', () => {
+			expect(array.indexOf(1)).to.eq(0);
+		});
+	});
 });
