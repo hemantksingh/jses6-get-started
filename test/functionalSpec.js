ca6882fdb5144d4abb1e1672a589ae8a327ec423
hemantksingh <hemantksingh.hk@gmail.com>
2016-01-30 19:06:06 +0000

    Test array destructuring.

--- test/functionalSpec.js
+++ test/functionalSpec.js
@@ -15,3 +15,37 @@ describe('Array indexes', () => {
 		});
 	});
 });
+
+
+describe('Destructuring', () => {
+	
+	describe('an array with 3 values', () => {
+		let [,x,y,z] = [3,1,2]
+
+		it('assigns the array values', () => {
+			expect(x).to.eq(1);
+			expect(y).to.eq(2);
+			expect(z).to.be.undefined;
+		});
+	});
+
+	describe('an array with 2 values', () => {
+		let x = 1;
+		let y = 2;
+		[x,y] = [y,x];
+
+		it('reverses the values', () => {
+			expect(x).to.eq(2);
+			expect(y).to.eq(1);
+		});
+	});
+
+	describe('an object', ()=>{
+		let {x, y} = {x: 1, y: 2};
+
+		it('assigns the values', () => {
+			expect(x).to.eq(1);
+			expect(y).to.eq(2);
+		});
+	});
+});
