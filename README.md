e4dadfc5c3c44a7619848b1a8333b56e2c4acd9d
hemantksingh <hemantksingh.hk@gmail.com>
2016-01-31 23:19:44 +0000

    Adds a start script for running the app.

--- README.md
+++ README.md
@@ -2,14 +2,18 @@ A sample app leveraging the ECMAScript 6 Features.
 
 ##Building the app##
 
-We are using *Gulp* as the build tool therefore the app defines a `gulpfile.js` to define build and test tasks. In order to build the app use `npm run build`. We use *Babel* compiler in order to compile ES2015 to ES5. This allows us to use the ES6 syntax, right now without waiting for ES6 support.
+`npm run build`
+
+We are using *Gulp* as the build tool therefore the app defines a `gulpfile.babel.js` to define build and test tasks.  We use *Babel* compiler in order to compile ES2015 to ES5. This allows us to use the ES6 syntax, right now without waiting for ES6 support.
 
 **Sourcemaps** plugin maps the compiled JS/CSS to source JS/CSS to fully track selectors and rules back to its very origin in your debugger.
 
 
 ##Running the app##
 
-TO run the app locally `babel-node index.js`. This compiles and runs the app on the fly.
+`npm run start`
+
+This compiles the ES6 modules into ES5 and then runs the app. However,if you have babel-node installed, `babel-node index.js` compiles and runs the app on the fly.
 
 ##Running tests ##
 
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,27 +3,34 @@ require('babel-core/register');
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import sourceMaps from 'gulp-sourcemaps';
+import merge from 'merge-stream';
 import mocha from 'gulp-mocha';
 
 gulp.task('test', ()=> {
 	return gulp.src('test/**/*.js', {read: false})
         .pipe(mocha({
             reporter: 'spec',
-            // require: [
-            //     './lib/global'
-            // ],
             compilers: {
                 js: babel
             }
         }));
 });
 
-gulp.task("build", () => {
-	return gulp.src("index.js")
-    	.pipe(sourceMaps.init())
-    	.pipe(babel({
-    		presets: ['es2015']
-    	}))
-    	.pipe(sourceMaps.write('.'))
-    	.pipe(gulp.dest("dist"));
+gulp.task('build', () => {
+	
+    let index = gulp.src('index.js')
+        .pipe(sourceMaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(sourceMaps.write('.'))
+        .pipe(gulp.dest('dist'));
+
+    let src = gulp.src('src/**/*', {base: '.'})
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist'));
+
+    return merge(index, src);    
 });
